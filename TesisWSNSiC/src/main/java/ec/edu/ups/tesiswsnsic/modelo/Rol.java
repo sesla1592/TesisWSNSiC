352ef1cfb16195498f221e068e5700893e46e020
rommel.inga <rommel.inga@GYE-OPE-ROI.AKROSATA.INT>
2018-08-28 03:01:04 -0500

    commit 4 by sesla

---
+++ TesisWSNSiC/src/main/java/ec/edu/ups/tesiswsnsic/modelo/Rol.java
@@ -0,0 +1,43 @@
+package ec.edu.ups.tesiswsnsic.modelo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="ROL")
+public class Rol {
+	
+	@Id
+	@Column(name="rol_id")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private int id;
+	
+	@Column(name="rol_descripcion")
+	private String descripcion;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	@Override
+	public String toString() {
+		return "Rol [id=" + id + ", descripcion=" + descripcion + "]";
+	}
+	
+}
