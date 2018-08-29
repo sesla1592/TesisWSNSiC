5a57bfba10398c97d848729d65372181906738c0
rommel.inga <rommel.inga@GYE-OPE-ROI.AKROSATA.INT>
2018-08-28 23:48:56 -0500

    change people

--- TesisWSNSiC/src/main/java/ec/edu/ups/tesiswsnsic/modelo/Persona.java
+++ TesisWSNSiC/src/main/java/ec/edu/ups/tesiswsnsic/modelo/Persona.java
@@ -1,10 +1,14 @@
 package ec.edu.ups.tesiswsnsic.modelo;
 
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -21,7 +25,23 @@ public class Persona {
 	
 	@Column(name="per_apellido")
 	private String apellido;
-
+	
+	@Column(name="per_estado")
+	private String estado;
+	
+	@Column(name="per_correo")
+	private String correo;
+	
+	@Column(name="per_password")
+	private String password;
+	
+	@OneToMany(cascade=(javax.persistence.CascadeType.ALL),fetch=FetchType.LAZY)
+	@JoinColumn(name="per_mov_fk", referencedColumnName="per_id")
+	private List<Movimiento> movimientos;
+	
+	@OneToMany(cascade=(javax.persistence.CascadeType.ALL),fetch=FetchType.LAZY)
+	@JoinColumn(name="per_nod_fk", referencedColumnName="per_id")
+	private List<PersonaNodo> personanodos;
 
 	public int getId() {
 		return id;
