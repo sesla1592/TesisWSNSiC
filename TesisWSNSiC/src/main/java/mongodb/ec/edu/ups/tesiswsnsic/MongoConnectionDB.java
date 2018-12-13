b38bdc5b0e402c2c31b78c406256540a92c218e4
RicardoPozo <pozopozothesituation@gmail.com>
2018-12-12 21:58:18 -0500

    Mongo Conexion....+ Pom.xml

--- TesisWSNSiC/src/main/java/mongodb/ec/edu/ups/tesiswsnsic/MongoConnectionDB.java
+++ TesisWSNSiC/src/main/java/mongodb/ec/edu/ups/tesiswsnsic/MongoConnectionDB.java
@@ -13,46 +13,41 @@ import com.mongodb.client.model.Aggregates;
 import com.mongodb.client.model.Projections;
 import com.mongodb.client.model.UnwindOptions;
 import com.mongodb.client.model.Filters;
-
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import org.bson.Document;
 
 public class MongoConnectionDB {
 
-	public static void main(String[] args) throws UnknownHostException {
+	private static String djson;
  
 
-//		/**Conexion a mongodb */			
-//		//MongoClient mongoClient = new MongoClient("localhost",27017);
-//		/**BD a elegir */	
-//		DB db=mongoClient.getDB("wsnbd"); 
-//		/**Lista de colecciones de la BD */	
-//		Set<String> collection=db.getCollectionNames(); 
-//
-//		/**Recorrer las colecciones y mostrar */	
-//		for (String s : collection) {
-//			System.out.println(s);
-//			}
-//		/**Ver documentos de una coleccion */	
-//		DBCollection coll = db.getCollection("Nueva");
-//		
+	public String recuperaDatos() {
 		
+	
 		String  nodo ="n2";
     	String  sensor ="d1";
-    	String  medicion ="temperatura";
-    	
+    	String  medicion ="humedad";
     	 Block<Document> printBlock = new Block<Document>() {
     	        @Override
+    	        
     	        public void apply(final Document document) {
+    	        	
     	            System.out.println(document.toJson());
+    	            djson = document.toJson();
+    	            JsonObject jsonObject = new JsonParser().parse(djson).getAsJsonObject();
+    	            String medici = jsonObject.getAsJsonObject("mediciones").get("medicion").getAsString();
+    	            String valor = jsonObject.getAsJsonObject("mediciones").get("valor").getAsString();
+    	            System.out.println(medici);
+    	            System.out.println(valor);
     	        }
     	    }; 
     	
     	MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
     	MongoDatabase database = mongoClient.getDatabase("wsnbd");
-    	MongoCollection<Document> collection = database.getCollection("Nueva");
-
-//    	
+    	MongoCollection<Document> collection = database.getCollection("Nueva");   	
     	collection.aggregate(
     		      Arrays.asList(
     		              Aggregates.match(Filters.eq("nombreNodo", nodo)),
@@ -73,6 +68,7 @@ public class MongoConnectionDB {
 		
 
 		System.out.println("Connection Succefull");
+		return null;
 
 	}
 
