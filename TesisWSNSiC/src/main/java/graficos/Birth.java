f301f35b8db0ac81b90180305f8bab03b030c5cb
wrocano <wrocano546@gmail.com>
2018-12-14 21:03:55 -0500

    ejemplo de graficas pagina: /sample.jsf

---
+++ TesisWSNSiC/src/main/java/graficos/Birth.java
@@ -0,0 +1,31 @@
+package rockstyle.controller.report;
+
+public class Birth {
+	 
+	  protected String year;
+	  protected Integer amount;
+	 
+	  public String getYear() {
+	    return year;
+	  }
+	 
+	  public void setYear(String year) {
+	    this.year = year;
+	  }
+	 
+	  public Integer getAmount() {
+	    return amount;
+	  }
+	 
+	  public void setAmount(Integer amount) {
+	    this.amount = amount;
+	  }
+	 
+	  public Birth() {
+	  }
+	 
+	  public Birth(String year, Integer amount) {
+	    this.year = year;
+	    this.amount = amount;
+	  }
+	}
