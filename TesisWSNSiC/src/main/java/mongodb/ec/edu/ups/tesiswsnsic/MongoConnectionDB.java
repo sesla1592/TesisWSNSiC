5f00ea6c8abd3f596268c881bb14385d19600b4c
rommel.inga <rommel.inga@GYE-OPE-ROI.AKROSATA.INT>
2018-12-19 08:27:15 -0500

    Commit with graphics: GOOGLE MAPS by pc sesla

--- TesisWSNSiC/src/main/java/mongodb/ec/edu/ups/tesiswsnsic/MongoConnectionDB.java
+++ TesisWSNSiC/src/main/java/mongodb/ec/edu/ups/tesiswsnsic/MongoConnectionDB.java
@@ -1,35 +1,114 @@
 package mongodb.ec.edu.ups.tesiswsnsic;
 
-import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 
 import com.mongodb.Block;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoIterable;
 import com.mongodb.client.model.Aggregates;
 import com.mongodb.client.model.Projections;
+import com.mongodb.client.model.Sorts;
 import com.mongodb.client.model.UnwindOptions;
+
+import modelo.ec.edu.ups.tesiswsnsic.MedValFec;
+
 import com.mongodb.client.model.Filters;
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.primefaces.model.map.DefaultMapModel;
+import org.primefaces.model.map.LatLng;
+import org.primefaces.model.map.MapModel;
+import org.primefaces.model.map.Marker;
+import org.springframework.data.domain.Sort;
 
 @ManagedBean
+@RequestScoped
 public class MongoConnectionDB {
- 
+
+	
+	public MapModel simpleModel;
+	
+	public String typemedic = "line";
+	protected List<MedValFec> puntos;
+	
+	public String medici;
+	public double val;
+	public String fec;
+	public String filtroBusqueda="hum";
+
 	private static String djson;
+
+	/* TEMPORALMENTE ESTA COLOCADO VALORES, ESTO DEBE PASARSE COMO PARAMETRO
+	 * */
+	String nodo ="n2";
+	String sensor ="d1";
+	String medicion ="hum";
+
+	public MongoConnectionDB() {
+		puntos = new ArrayList<>();
+		simpleModel = new DefaultMapModel();
+		recuperaDatos();
+		graficarMapa();
+		
+	}
 	
-	private String nodo;
-	private String sensor;
-	private String medicion;
+	public String getTypemedic() {
+		return typemedic;
+	}
+
+	public void setTypemedic(String typemedic) {
+		this.typemedic = typemedic;
+	}
 	
-	
+	public String getMedici() {
+		return medici;
+	}
+
+	public void setMedici(String medici) {
+		this.medici = medici;
+	}
+
+	public String getFec() {
+		return fec;
+	}
+
+	public void setFec(String fec) {
+		this.fec = fec;
+	}
+
+	public double getVal() {
+		return val;
+	}
+
+	public void setVal(double val) {
+		this.val = val;
+	}
+
+	public List<MedValFec> getPuntos() {
+		return puntos;
+	}
+
+	public void setPuntos(List<MedValFec> puntos) {
+		this.puntos = puntos;
+	}
+
 	public String getNodo() {
 		return nodo;
 	}
@@ -41,61 +120,116 @@ public class MongoConnectionDB {
 	public String getSensor() {
 		return sensor;
 	}
+
 	public void setSensor(String sensor) {
 		this.sensor = sensor;
 	}
+
 	public String getMedicion() {
 		return medicion;
 	}
+
 	public void setMedicion(String medicion) {
 		this.medicion = medicion;
 	}
 
-	public String recuperaDatos() {
-		
-	
-		//String  nodo ="n2";
-    	//String  sensor ="d1"; 
-    	//String  medicion ="hum";
-		
-    	 Block<Document> printBlock = new Block<Document>() {
-    	        @Override
-    	        
-    	        public void apply(final Document document) {
-    	        	
-    	            System.out.println(document.toJson());
-    	            djson = document.toJson();
-    	            JsonObject jsonObject = new JsonParser().parse(djson).getAsJsonObject();
-    	            String medici = jsonObject.getAsJsonObject("mediciones").get("medicion").getAsString();
-    	            String valor = jsonObject.getAsJsonObject("mediciones").get("valor").getAsString();
-    	            System.out.println(medici);
-    	            System.out.println(valor);
-    	        }
-    	    }; 
-    	
-    	MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
-    	MongoDatabase database = mongoClient.getDatabase("DBWSNSIN");
-    	MongoCollection<Document> collection = database.getCollection("Nueva");   	
-    	collection.aggregate(
-    		      Arrays.asList(
-    		              Aggregates.match(Filters.eq("nombreNodo", nodo)),
-    		              Aggregates.unwind( "$mediciones", new UnwindOptions().preserveNullAndEmptyArrays(true)),
-    		              Aggregates.match(Filters.eq("mediciones.nombreSensor", sensor)),
-    		              Aggregates.match(Filters.eq("mediciones.medicion", medicion)),
-    		              Aggregates.project(
-    		                      Projections.fields(
-    		                            Projections.excludeId(),
-    		                            Projections.include("mediciones.medicion"),
-    		                            Projections.include("mediciones.valor"),
-    		                            Projections.include("mediciones.fecha")
-    		                      )
-    		                  )
-    		             
-    		      )
-    		).forEach(printBlock);
-		System.out.println("Connection Succefull");
-		return null;
+	public String getFiltroBusqueda() {
+		return filtroBusqueda;
+	}
 
+	public void setFiltroBusqueda(String filtroBusqueda) {
+		this.filtroBusqueda = filtroBusqueda;
+	}
+
+	public void LineChartBean() {
+		recuperaDatos();
+	}
+	
+	
+	public MapModel getSimpleModel() {
+		return simpleModel;
+	}
+
+	public void setSimpleModel(MapModel simpleModel) {
+		this.simpleModel = simpleModel;
+	}
+
+	public void mapa(double lat, double longi,String nombre) {
+		System.out.println("datos q llegan "+lat+","+longi+","+nombre);
+        
+        //Shared coordinates
+        LatLng coord = new LatLng(lat,longi);
+        
+          
+        //Basic marker
+        simpleModel.addOverlay(new Marker(coord, nombre));
+        System.out.println("agrego");
+	}
+	
+	public void graficarMapa() {
+		
+		for (int i = 0; i < puntos.size(); i++) {
+			mapa(puntos.get(i).getLatitud(), puntos.get(i).getLongitud(), "sensor "+i);
+		}
+	}
+	public String recuperaDatos() {
+
+		Block<Document> printBlock = new Block<Document>() {
+			@Override
+
+			public void apply(final Document document) {
+				djson = "[" +document.toJson()+ "]";
+//				System.out.println(djson);
+				JsonParser parser = new JsonParser();
+				JsonArray gsonArr = parser.parse(djson).getAsJsonArray();
+				for(JsonElement obj : gsonArr) {	
+					
+					JsonObject gsonObj = obj.getAsJsonObject();
+					//ELEMENTOS PRIMITIVOS DE OBJETOS: fecha
+					fec = gsonObj.get("fecha").getAsString();
+					//ELEMENTOS DENTRO DEL LISTADO DE PRIMITIVO: valor medicion
+					//JsonObject gsonObj2= (JsonObject) gsonObj.get("mediciones");
+					
+					JsonArray ltsMediciones = (JsonArray) gsonObj.get("mediciones");
+					for (int i = 0; i < ltsMediciones.size(); i++) {
+						
+						JsonObject gsonObj2= (JsonObject) ltsMediciones.get(i);
+						medici = gsonObj2.get("medicion").getAsString();
+						if(medici.equals(filtroBusqueda)) {
+							val = gsonObj2.get("valor").getAsDouble();
+							double lat=Double.parseDouble(gsonObj2.get("latitud").getAsString());
+							double longi=Double.parseDouble(gsonObj2.get("longitud").getAsString());
+							puntos.add(new MedValFec(medici, val, fec,lat,longi));
+							
+						}
+					}
+				}
+//				for(int i=0;i<puntos.size();i++) {
+//					System.out.println("POINTS   "+puntos.get(i).getFecha()+" - "+puntos.get(i).getMedicion()+" - "+puntos.get(i).getValor());
+//				}
+				
+			}
+		};
+		MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
+		System.out.println("MCLI");
+		MongoDatabase database = mongoClient.getDatabase("DBWSNSIN");
+		System.out.println("DBWSSIN");
+		FindIterable<Document> collection = database.getCollection("Nueva").find().sort(Sorts.descending("fecha")).limit(2);
+		 
+		collection.forEach(printBlock); 
+		
+//		collection
+//				.aggregate(Arrays.asList(Aggregates.match(Filters.eq("nombreNodo", nodo)),
+//						Aggregates.unwind("$mediciones", new UnwindOptions().preserveNullAndEmptyArrays(true)),
+//						Aggregates.match(Filters.eq("mediciones.nombreSensor", sensor)),
+//						Aggregates.match(Filters.eq("mediciones.medicion", medicion)),
+//						
+//						Aggregates.project(Projections.fields(Projections.excludeId(),
+//								Projections.include("mediciones.medicion"), Projections.include("mediciones.valor"),
+//								Projections.include("fecha")))
+//				)).forEach(printBlock);
+		System.out.println("Connection Succesfull");
+		return null;
 	}
 
 }
