f1c1564ff815c883c58ba1146017db207837346c
iwia <iwia@DESKTOP-R9A4SNH>
2019-05-28 20:28:32 -0500

    Changes design by sesla

--- TesisWSNSiC/src/main/java/validacionesnegocio/ec/edu/ups/tesiswsnsic/AuthorizationFilter.java
+++ TesisWSNSiC/src/main/java/validacionesnegocio/ec/edu/ups/tesiswsnsic/AuthorizationFilter.java
@@ -77,6 +77,12 @@ public class AuthorizationFilter implements Filter {
 					// || reqURI.contains("javax.faces.resource")
 					) {
 				chain.doFilter(request, response);
+			} else if (reqURI.indexOf("/faces/login/resetPassword.xhtml") >= 0
+					// || (ses == null && ses.getAttribute("username") == null)
+					// || reqURI.indexOf("/public/") >= 0
+					// || reqURI.contains("javax.faces.resource")
+					) {
+				chain.doFilter(request, response);
 			}
 
 			else if (reqURI.indexOf("/faces/login/Login.xhtml") >= 0
